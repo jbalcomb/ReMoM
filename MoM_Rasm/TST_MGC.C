cb28d67bda72a2c1aaee9cddfa23f368b6399f91
Jim Balcomb <45997204+jbalcomb@users.noreply.github.com>
2022-11-23 20:40:43 -0500

    moved STU to ./STU

--- MoM_Rasm/TST_MGC.C
+++ MoM_Rasm/TST_MGC.C
@@ -182,9 +182,9 @@ int main(int argc, char *argv[])
     // // test_FLIC_Draw_XY();
 
     // test_MGC_Main();
-    // MGC_main(argc, argv);
+    MGC_main(argc, argv);
     // test_WZD_Main();
-    WZD_main(argc, argv);
+    // WZD_main(argc, argv);
 
     // test_wait_for_esc();
     // test_TST_Test_Screen();
