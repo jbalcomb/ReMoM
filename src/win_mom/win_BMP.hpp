ac256f5328a8a3657917e586aae5d5d9d3a5fd5b
Jim Balcomb <45997204+jbalcomb@users.noreply.github.com>
2022-12-08 14:37:24 -0500

    trash 'win_mom'

--- src/win_mom/win_BMP.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef WIN_BMP_HPP
-#define WIN_BMP_HPP
-
-
-void STU_Export_VBB_To_BMP32(void);
-
-
-#endif /* WIN_BMP_HPP */
--- src/win_mom/win_BMP_02.cpp
+++
@@ -1,7 +0,0 @@
-
-/*
-
-win_BMP_02
-https://croakingkero.com/tutorials/load_bmp/
-
-*/
