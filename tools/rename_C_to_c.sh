7a69030c86b25f237e97104cdcfc2ad60164066e
Jim Balcomb <jim.balcomb@gmail.com>
2025-07-12 12:38:01 -0400

    rename .C/.H to .c/.h
    some (dumb) tools (still) think .C/.H is C++

--- tools/rename_C_to_c.sh
+++ tools/rename_C_to_c.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Loop through all files ending with .C (case-sensitive)
-for file in *.C; do
+for file in *.c; do
   # Check if the file exists (to handle cases where no .C files are found)
   if [ -f "$file" ]; then
     # Construct the new filename by replacing .C with .c
-    new_file="${file%.C}.c"
+    new_file="${file%.c}._c_"
     # Rename the file
     mv "$file" "$new_file"
     echo "Renamed '$file' to '$new_file'"
--- tools/rename_H_to_h.sh
+++ tools/rename_H_to_h.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Loop through all files ending with .C (case-sensitive)
-for file in *.H; do
+for file in *.h; do
   # Check if the file exists (to handle cases where no .C files are found)
   if [ -f "$file" ]; then
     # Construct the new filename by replacing .C with .c
-    new_file="${file%.H}.h"
+    new_file="${file%.h}._h_"
     # Rename the file
     mv "$file" "$new_file"
     echo "Renamed '$file' to '$new_file'"
